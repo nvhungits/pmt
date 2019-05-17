febc2f1a54285bf1f378bd5aaa969b76f4dc1d86
nvhungits@gmail.com <nvhungits@gmail.com>
2019-05-18 00:02:02 +0700

    big updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { TopbarComponent } from './topbar/topbar.component';
+import { SliderComponent } from './slider/slider.component';
+import { HomeComponent } from './home/home.component';
+import { ProductSaleComponent } from './product-sale/product-sale.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TopbarComponent
+    TopbarComponent,
+    SliderComponent,
+    HomeComponent,
+    ProductSaleComponent,
+    ProductListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
